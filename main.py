62abdcce075183685a96451d129d30222442cc3e
Sagar <sagarmoysengupta@gmail.com>
2023-05-17 13:09:08 +0530

    Removing cuda

--- main.py
+++ main.py
@@ -12,19 +12,18 @@ from io import BytesIO
 model_name = "t5-small"
 tokenizer = T5Tokenizer.from_pretrained(model_name)
 model = T5ForConditionalGeneration.from_pretrained(
-    model_name).to("cuda")
+    model_name)
 
 pipe = StableDiffusionPipeline.from_pretrained(
     "stabilityai/stable-diffusion-2-1-base", revision="fp16", torch_dtype=torch.float16)
-pipe.to("cuda")
+pipe
 
-# model_id = "stabilityai/stable-diffusion-2-1-base"
+model_id = "stabilityai/stable-diffusion-2-1-base"
 
-# scheduler = EulerDiscreteScheduler.from_pretrained(
-#     model_id, subfolder="scheduler")
-# pipe = StableDiffusionPipeline.from_pretrained(
-#     model_id, scheduler=scheduler, torch_dtype=torch.float16)
-# pipe = pipe.to("cuda")
+scheduler = EulerDiscreteScheduler.from_pretrained(
+    model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(
+    model_id, scheduler=scheduler, torch_dtype=torch.float16)
 
 # Start flask app and set to ngrok
 app = Flask(__name__)
@@ -43,15 +42,15 @@ def generate():
     print(f"Generating an image of {prompt}")
 
     # generate image
-    # image = pipe(prompt).images[0]
-    # print("Image generated! Converting image ...")
-    # buffered = BytesIO()
-    # image.save(buffered, format="PNG")
-    # img_str = base64.b64encode(buffered.getvalue())
-    # img_str = "data:image/png;base64," + str(img_str)[2:-1]
+    image = pipe(prompt).images[0]
+    print("Image generated! Converting image ...")
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue())
+    img_str = "data:image/png;base64," + str(img_str)[2:-1]
 
     # generate text
-    input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
     generated_output = model.generate(
         input_ids, do_sample=True, temperature=1.0, max_length=2500, num_return_sequences=1)
     generated_text = tokenizer.decode(
