e188f01c6b34afafae8d42387239e296a14f3f73
Sagar <sagarmoysengupta@gmail.com>
2023-05-17 12:44:17 +0530

    Changing smaller model

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ import base64
 from io import BytesIO
 
 # Load model
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
+model_name = "t5-small"
+tokenizer = T5Tokenizer.from_pretrained(model_name)
 model = T5ForConditionalGeneration.from_pretrained(
-    "google/flan-t5-small").to("cuda")
+    model_name).to("cuda")
 
 pipe = StableDiffusionPipeline.from_pretrained(
     "stabilityai/stable-diffusion-2-1-base", revision="fp16", torch_dtype=torch.float16)
 pipe.to("cuda")
 
-model_id = "stabilityai/stable-diffusion-2-1-base"
+# model_id = "stabilityai/stable-diffusion-2-1-base"
 
-scheduler = EulerDiscreteScheduler.from_pretrained(
-    model_id, subfolder="scheduler")
-pipe = StableDiffusionPipeline.from_pretrained(
-    model_id, scheduler=scheduler, torch_dtype=torch.float16)
-pipe = pipe.to("cuda")
+# scheduler = EulerDiscreteScheduler.from_pretrained(
+#     model_id, subfolder="scheduler")
+# pipe = StableDiffusionPipeline.from_pretrained(
+#     model_id, scheduler=scheduler, torch_dtype=torch.float16)
+# pipe = pipe.to("cuda")
 
 # Start flask app and set to ngrok
 app = Flask(__name__)
@@ -42,12 +43,12 @@ def generate():
     print(f"Generating an image of {prompt}")
 
     # generate image
-    image = pipe(prompt).images[0]
-    print("Image generated! Converting image ...")
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    img_str = base64.b64encode(buffered.getvalue())
-    img_str = "data:image/png;base64," + str(img_str)[2:-1]
+    # image = pipe(prompt).images[0]
+    # print("Image generated! Converting image ...")
+    # buffered = BytesIO()
+    # image.save(buffered, format="PNG")
+    # img_str = base64.b64encode(buffered.getvalue())
+    # img_str = "data:image/png;base64," + str(img_str)[2:-1]
 
     # generate text
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
@@ -58,7 +59,7 @@ def generate():
 
     print("Sending image and text ...")
 
-    return render_template('index.html', generated_image=img_str, generated_text=generated_text, prompt=prompt)
+    return render_template('index.html', generated_image="", generated_text=generated_text, prompt=prompt)
 
 
 if __name__ == '__main__':
